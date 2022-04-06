6dd42b61d5fe782a9ea15bb49afcf7c975c41c3d
Rodrigo S Brasil <maic_rodrigo@hotmail.com>
2022-04-06 10:06:35 -0300

    dialog contacts

--- src/UI/Widgets/Dialogs/CreateBoardDialog.js
+++ src/UI/Widgets/Dialogs/CreateBoardDialog.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, Checkbox, FormControlLabel, TextField, Divider, Typography } from '@mui/material';
+import { Dialog, DialogTitle, Stack, DialogContent, DialogContentText, Checkbox, FormControlLabel, TextField, Divider, Typography } from '@mui/material';
 import { People } from '@mui/icons-material';
 
 import Button from '../Buttons/Button';
+import { Box } from '@material-ui/core';
 
 
 export const CreateBoardDialog = ({ open, onClose }) => {
@@ -12,59 +13,84 @@ export const CreateBoardDialog = ({ open, onClose }) => {
         <Dialog
             open={open}
             onClose={onClose}
-            aria-labelledby="form-dialog-save-board"
+            aria-labelledby='form-dialog-save-board'
             fullWidth
-            maxWidth="md"
-
+            maxWidth='sm'
         >
             <DialogTitle
-                sx={{ textAlign: 'center' }}
-                fontWeight='bold'
+                sx={{
+                    textAlign: 'center',
+                }}
+                fontWeight='700'
             >
                 Create Board
             </DialogTitle>
-            <Divider />
 
             <DialogContent>
+                <Divider
+                    sx={{
+                        mb: 3,
+                    }}
+                />
 
                 <TextField
-                    margin="dense"
-                    id="name"
-                    label="Board Name"
-                    type="text"
+                    margin='dense'
+                    id='name'
+                    label='Create Board Name'
+                    type='text'
                     fullWidth
+                    InputProps={{
+                        sx: {
+                            fontSize: '1.2rem',
+                            fontWeight: 'bold',
+                        }
+                    }}
+                    InputLabelProps={{
+                        sx: {
+                            fontWeight: 'bold',
+                            color: 'text.disabled',
+                        }
+                    }}
                 />
 
+                <Typography py={2} fontWeight='bold' fontSize='12px' color='text.secondary' >
+                    Filter Criteria:
+                </Typography>
+                <Divider
+                    sx={{
+                        my: 2,
+                    }}
+                />
+
+                <Stack direction='row' gap={2}>
+                    <FormControlLabel
+                        sx={{
+                            mr: 'auto',
+                            alignSelf: 'center',
+                        }}
+                        control={
+                            <Checkbox
+                                defaultChecked
+                                name='share'
+                                color='primary'
+                            />
+                        }
+                        label='Share with team'
+                    />
+
+                    <Button
+                        variant='outlined'
+                        onClick={onClose}
+                        name='Cancel'
+                    />
+                    <Button
+                        variant='contained'
+                        type='submit'
+                        endIcon={<People />}
+                        name='Create Board'
+                    />
+                </Stack>
             </DialogContent>
-            <DialogActions>
-
-                <FormControlLabel
-                    sx={{ mr: 'auto' }}
-                    control={
-                        <Checkbox
-                            checked={false}
-                            onChange={() => { }}
-                            name="checkedB"
-                            color="primary"
-                        />
-                    }
-                    label="Shere with team"
-                />
-
-
-                <Button
-                    variant="outlined"
-                    onClick={onClose}
-                    name='Cancel'
-                />
-                <Button
-                    variant="contained"
-                    type='submit'
-                    endIcon={<People />}
-                    name='Create Board'
-                />
-
-            </DialogActions>
         </Dialog>
 
     )
