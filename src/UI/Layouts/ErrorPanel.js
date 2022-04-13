d30c27f4dae3d08a091b7d4e8f41ad9c31460abc
gabsschneider <gaschneider2008@hotmail.com>
2022-04-13 13:48:45 -0300

    Added ErrorPanel

---
+++ src/UI/Layouts/ErrorPanel.js
@@ -0,0 +1,48 @@
+
+import { Grid, Container, Stack, Typography } from '@mui/material';
+
+
+export default function ErrorPanel(props) {
+
+    // if(props.body) {
+    //     Object.keys(props.body).forEach(key => {
+    //         console.log(key)
+    //         console.log(props.body[key])
+    //     })
+    // }
+
+    return (
+        <Grid 
+          container
+          alignItems="center"
+          justifyContent="center"
+          >
+            <Stack mt={5} alignItems="center">
+                <span style={{...styles.span }}>
+                    Sorry, something bad happened :(
+                </span>
+                <span style={{...styles.span, marginBottom: 60}}>
+                    When trying to access your message, we got the following error:
+                </span>
+                <h1 style={{ fontWeight: 'bold', textAlign: 'center', color: '#555' }}>{props.title}</h1>
+                <span style={{...styles.span, fontSize: 26, color: '#555', maxWidth: 400 }}>
+                    {props.body}
+                </span>
+            </Stack>
+        </Grid>
+
+    )
+}
+
+const styles = {
+    container: {
+        flex: 1,
+    },
+    span: {
+        fontSize: 22,
+        color: '#bbb',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        maxWidth: 300
+    }
+}
