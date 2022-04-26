b5c35822a129f988ffbc6cf0ddce9a7a4c83e46b
jardel978 <jardeloliver197@gmail.com>
2022-04-26 17:10:43 -0300

    created UserSettingsAccountPage

---
+++ src/Pages/Settings/UserSettingsProfilePage.js
@@ -0,0 +1,17 @@
+import UserSettingsPage from "./UserSettingsPage";
+
+const UserSettingsProfilePage = (props) => {
+
+
+  return (
+    <UserSettingsPage
+      title='Profile'
+    // topActionName='+ New Grad Year'
+    // onTopActionClick={onTopActionClick}
+    >
+      Profile
+    </UserSettingsPage>
+  )
+}
+
+export default UserSettingsProfilePage;
