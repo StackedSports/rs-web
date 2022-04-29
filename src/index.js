bf1f6bd44e7b733c340a4c39b1410f670ff3ef93
gabsschneider <gaschneider2008@hotmail.com>
2022-04-29 08:42:34 -0300

    Added MUI Pro license key

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from "./App";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { LicenseInfo } from '@mui/x-license-pro';
+
+LicenseInfo.setLicenseKey(
+  '83acb3ef467a6195c10e23f5ffb39f3fT1JERVI6NDAwMDUsRVhQSVJZPTE2NzkyNjU3OTkwMDAsS0VZVkVSU0lPTj0x',
+);
+
 ReactDOM.render(
     <Router>
         <App/>
