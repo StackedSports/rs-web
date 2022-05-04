774d6960a84240eb29d36dcfb9db594833a17c4e
gabsschneider <gaschneider2008@hotmail.com>
2022-05-04 08:17:24 -0300

    Contact Profile Page media details implementation

--- src/UI/Widgets/Contact/ContactMessageDetails.js
+++ src/UI/Widgets/Contact/ContactMessageDetails.js
@@ -7,17 +7,33 @@ import ContactAssociatedMedia from './ContactAssociatedMedia';
 
 const ContactMessageDetails = (prosp) => {
 
-  const onRefresh = () => {
-    console.log("onRefresh")
-  }
+	const onRefresh = () => {
+		console.log("onRefresh")
+	}
 
-  return (
-    <Stack sx={{ width: "300px" }} spacing={1} alignItems="center" justifyContent="center">
-      <ContactMessageStats />
-      <ContactSentMedia />
-      <ContactAssociatedMedia />
-    </Stack>
-  )
+	const onViewMore = (id) => {
+		// setSelectedMedia(id)
+	}
+
+	return (
+		<Stack sx={{ width: "300px" }} spacing={1} alignItems="center" justifyContent="center">
+			<Stack >
+				<ContactMessageStats />
+				<ContactMediaPreview
+				  id="associated" 
+				  media={contact.associated_media} 
+				  onViewMore={onViewMore}
+				/>
+				<ContactMediaPreview media={contact.sent_media}/>
+				{/* <ContactSentMedia />
+				<ContactAssociatedMedia /> */}
+			</Stack>
+			<Stack >
+				{selected === 'associated' && <ContactMediaDetails media={contact.associated_media}/>}
+				<ContactMediaDetails media={contact.sent_media}/>
+			</Stack>
+		</Stack>
+	)
 }
 
 export default ContactMessageDetails;
