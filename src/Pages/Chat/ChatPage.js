b4836c2492cd81d1b68f36c0424bd3b6b286e6e9
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2022-06-22 16:36:55 -0300

    chat page improve ui

--- src/Pages/Chat/ChatPage.js
+++ src/Pages/Chat/ChatPage.js
@@ -1,10 +1,12 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useCallback, useEffect } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
-import { ListItemButton, Stack, Avatar, List, ListItem, Typography, Grid, Box } from "@mui/material";
+import { ListItemButton, Stack, Avatar, List, ListItem, Typography, Grid, Box, IconButton } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import TuneIcon from '@mui/icons-material/Tune';
 import CloseIcon from '@mui/icons-material/Close';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
 
 import Page, { Content } from 'UI/Layouts/Page';
 import Panel from 'UI/Layouts/Panel';
@@ -15,15 +17,48 @@ import SideFilter from 'UI/Widgets/SideFilter';
 import SearchBar from 'UI/Widgets/SearchBar';
 // import LoadingOverlay from 'UI/Widgets/LoadingOverlay'
 
+import { useLocalStorage } from 'Hooks/useLocalStorage';
 import ConfirmDialogContext from 'Context/ConfirmDialogProvider';
+import { AuthContext } from 'Context/Auth/AuthProvider';
 import ConversationChat from './Components/ConversationChat';
 
+// Data to test
+const conversations = [
+  {
+    id: '0',
+    name: 'Luke Burke 1',
+    messages: [
+      {
+        id: "0",
+      }
+    ]
+  },
+  {
+    id: '1',
+    name: 'Luke Burke 2',
+  },
+  {
+    id: '2',
+    name: 'Luke Burke 3',
+  },
+  {
+    id: '3',
+    name: 'Luke Burke 4',
+  },
+  {
+    id: '4',
+    name: 'Luke Burke 5',
+  },
+  {
+    id: '5',
+    name: 'Luke Burke 6',
+  },
+  {
+    id: '6',
+    name: 'Luke Burke 7',
+  },
+]
 
-const changeItemPosition = (arr, from, to) => {
-  let el = arr[from]
-  arr.splice(from, 1)
-  return arr.splice(to, 0, el);
-};
 
 const ChatListItem = (props) => {
 
@@ -44,24 +79,28 @@ const ChatListItem = (props) => {
       key={props.conversation?.id}
       onMouseEnter={() => setShowIconClose(true)}
       onMouseLeave={() => setShowIconClose(false)}
-      onClick={() => props.onViewConversation(props.conversation)}
+      onClick={() => props.onToggleChat(props.conversation)}
     >
       <Stack flex={1} direction="row" spacing={2} alignItems="center">
-        <CloseIcon sx={{
-          visibility: showIconClose ? "visible" : "hidden",
-          transition: "visibility .2s ease",
-          position: "absolute",
-          marginRight: "15px",
-          right: 0,
-          zIndex: 1,
-        }}
-          onClick={() => onArchiveConversation(props.conversation)}
-        />
+        <IconButton
+          sx={{
+            visibility: showIconClose ? "visible" : "hidden",
+            position: "absolute",
+            marginRight: "15px",
+            right: 0,
+            bottom: 0,
+            transform: "translateY(-50%)",
+            zIndex: 1,
+          }}
+          onClick={(e) => { e.stopPropagation(); onArchiveConversation(props.conversation) }}
+        >
+          <ArchiveIcon />
+        </IconButton>
         <Avatar style={{
           width: "56px",
           height: "56px",
         }}
-          aria-label="recipe"
+          aria-label="avatar"
           src='https://stakdsocial.s3.us-east-2.amazonaws.com/media/general/contact-missing-image.png'
         />
         <Stack flex={1} sx={{ position: "relative" }}>
@@ -69,18 +108,20 @@ const ChatListItem = (props) => {
             position: "absolute",
             right: 0,
           }}
-            variant="body2"
+            variant='body2'
+            fontWeight={600}
+            fontSize={12}
           >
-            <b>11:23 am</b>
+            11:23 am
           </Typography>
 
-          <Typography variant="body1">
-            <b>{props.conversation?.name}</b>
+          <Typography variant="body1" fontWeight='bold'>
+            {props.conversation?.name}
           </Typography>
-          <Typography variant="body2" color="#dadada">
+          <Typography variant="body2" color="text.secondary">
             @charles
           </Typography>
-          <Typography variant="body2" color="#dadada">
+          <Typography variant="body2" color="text.secondary">
             There Ways To Get Travel Disco...
           </Typography>
         </Stack>
@@ -91,9 +132,23 @@ const ChatListItem = (props) => {
 
 export default function ChatPage(props) {
   const confirmDialog = useContext(ConfirmDialogContext)
+  const { user } = useContext(AuthContext)
+  const [pinnedChats, setPinnedChats] = useLocalStorage('pinnedChats', {})
 
   const [displayFilters, setDisplayFilters] = useState(true)
   const [conversationViewer, setConversationViewer] = useState([])
+
+  useEffect(() => {
+    const pinned = conversations.filter(conversation => pinnedChats[user.id].includes(conversation.id))
+    setConversationViewer(pinned)
+  }, [])
+
+  const isPinned = useCallback((conversation) => {
+    if (pinnedChats[user.id]) {
+      return pinnedChats[user.id].includes(conversation.id)
+    }
+    return false
+  }, [pinnedChats, user.id])
 
   const onTopActionClick = () => {
     console.log("onTopActionClick")
@@ -104,14 +159,14 @@ export default function ChatPage(props) {
     setDisplayFilters(!displayFilters)
   }
 
-  const onViewConversation = (conversation) => {
+  const onToggleChat = (conversation) => {
+    console.log("open conversation")
     let index = 0
     const conv = conversationViewer.filter(conv => conv?.id === conversation.id && conversation)
     if (conv.length === 0) {
 
       setConversationViewer([conversation, ...conversationViewer])
     } else {
-      console.log("contains")
       // index = conversationViewer.indexOf(conversation)
       // console.log(conv)
     }
@@ -141,6 +196,39 @@ export default function ChatPage(props) {
     })
   }
 
+  const onPin = (conversation) => {
+    const newPinnedChats = { ...pinnedChats };
+    const conversationId = conversation.id;
+
+    newPinnedChats[user.id] = newPinnedChats[user.id] || [];
+    if (newPinnedChats[user.id].includes(conversationId)) {
+      newPinnedChats[user.id] = newPinnedChats[user.id].filter(id => id !== conversationId)
+    }
+    else {
+      newPinnedChats[user.id].push(conversationId)
+    }
+    setPinnedChats(newPinnedChats)
+  }
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = [...list];
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    // dropped outside the list
+    if (!destination)
+      return;
+
+    if (destination.index === source.index)
+      return;
+
+    setConversationViewer(reorder(conversationViewer, source.index, destination.index))
+  }
+
   const Icon = displayFilters ? MenuOpenIcon : MenuIcon
 
   const filters = [
@@ -151,42 +239,6 @@ export default function ChatPage(props) {
         { id: '0', name: '@BG615', /* path: contactsRoutes.all */ },
         { id: '1', name: '615.999.9999', /* path: contactsRoutes.all */ }
       ]
-    },
-  ]
-
-  const conversations = [
-    {
-      id: '0',
-      name: 'Luke Burke 1',
-      messages: [
-        {
-          id: "0",
-        }
-      ]
-    },
-    {
-      id: '1',
-      name: 'Luke Burke 2',
-    },
-    {
-      id: '2',
-      name: 'Luke Burke 3',
-    },
-    {
-      id: '3',
-      name: 'Luke Burke 4',
-    },
-    {
-      id: '4',
-      name: 'Luke Burke 5',
-    },
-    {
-      id: '5',
-      name: 'Luke Burke 6',
-    },
-    {
-      id: '6',
-      name: 'Luke Burke 7',
     },
   ]
 
@@ -204,14 +256,16 @@ export default function ChatPage(props) {
           collapsed={true}
         // onFilterSelected={onFilterSelected}
         />
-        <Panel hideHeader>
+        <Panel hideHeader sx={{ minWidth: 0 }}>
           <Grid container flex={1} flexWrap='nowrap' >
 
             <Grid item sx={{//conversation summary list
               width: "370px",
               overflowY: "auto",
+              overscrollBehaviorBlock: 'contain',
               border: "red solid 1px",
-              borderRadius: "5px",
+              borderEndStartRadius: '5px',
+              borderStartStartRadius: '5px',
               border: "solid 1px #dadada",
             }} >
               <Stack
@@ -240,8 +294,9 @@ export default function ChatPage(props) {
 
                 {conversations.map(conversation => (
                   <ChatListItem
+                    key={conversation.id}
                     conversation={conversation}
-                    onViewConversation={onViewConversation}
+                    onToggleChat={onToggleChat}
                     onArchiveConversation={onArchiveConversation}
                   />
                 ))
@@ -249,39 +304,39 @@ export default function ChatPage(props) {
               </List>
             </Grid>
 
-            //conversation details container
-            <Grid item xs container > 
-            
-              {conversationViewer.map(conversation => (
-                <ConversationChat
-                  conversation={conversation}
-                  onCloseConversation={onCloseConversation}
-                />
-              ))
-              }
-
-            </Grid>
-
-            {/* {alert && (
-            <Snackbar
-              anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-              open={props.alert.visible}
-              autoHideDuration={6000}
-              onClose={props.alert.dismiss}>
-              <MuiAlert
-                variant="filled"
-                onClose={props.alert.dismiss}
-                severity={props.alert.severity}
-                sx={{ width: '100%' }}
-              >
-                {props.alert.message}
-              </MuiAlert>
-            </Snackbar>
-            )} */}
-
-            {/* {props.redirect && props.redirect !== '' && <Redirect push to={props.redirect} />} */}
-
-            {/* {loading && <LoadingOverlay />} */}
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="droppable" direction="horizontal">
+                {(provided, snapshot) => (
+                  <Grid item xs  //conversation details container
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    sx={{
+                      borderEndEndRadius: '5px',
+                      borderStartEndRadius: '5px',
+                      display: 'grid',
+                      gridAutoFlow: 'column',
+                      gridAutoColumns: '450px',
+                      overflowX: 'auto',
+                      overscrollBehaviorInline: 'contain',
+                      gap: 1,
+                      minHeight: 0,
+                    }}
+                  >
+                    {conversationViewer.map((conversation, index) => (
+                      <ConversationChat
+                        isPinned={isPinned(conversation)}
+                        index={index}
+                        key={conversation.id}
+                        conversation={conversation}
+                        onCloseConversation={onCloseConversation}
+                        onPin={() => onPin(conversation)}
+                      />
+                    ))}
+                    {provided.placeholder}
+                  </Grid>
+                )}
+              </Droppable>
+            </DragDropContext>
 
           </Grid>
         </Panel>
--- src/Pages/Chat/Components/ConversationChat.js
+++ src/Pages/Chat/Components/ConversationChat.js
@@ -1,25 +1,33 @@
-import { useState } from 'react';
+import { useState, useRef, useContext } from 'react';
 import {
   Grid,
   Divider,
   Stack,
+  Paper,
   Checkbox,
   Avatar,
   Typography,
+  Tooltip,
   Box,
+  IconButton,
   List,
   ListItem,
+  TextField,
 } from "@mui/material";
-import MessageInput from 'UI/Forms/Inputs/MessageInput';
+import { Draggable } from 'react-beautiful-dnd';
+
 import CloseIcon from '@mui/icons-material/Close';
-import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
-import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
-import ContentCutIcon from '@mui/icons-material/ContentCut';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CircleUnchecked from "@material-ui/icons/RadioButtonUnchecked";
+import ShortTextIcon from '@mui/icons-material/ShortText';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
+import WrapTextIcon from '@mui/icons-material/WrapText';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 
 import Button from 'UI/Widgets/Buttons/Button';
 import { PanelDropdown } from 'UI/Layouts/Panel';
+import { EmojiPicker } from 'UI/Forms/Inputs/MessageInput';
 
 const TextMessage = (props) => {
 
@@ -46,7 +54,8 @@ const TextMessage = (props) => {
         />
       }
       <Typography style={{
-        padding: "10px",
+        margin: props.owner ? "0 0 0 30px" : "0 30px 0 0",
+        padding: '10px',
         color: props.owner ? "#fff" : "#000",
         backgroundColor: props.owner ? "#3871DA" : "#f1f0f0",
         borderRadius: props.owner ? "20px 20px 0 20px" : "20px 20px 20px 0",
@@ -54,8 +63,8 @@ const TextMessage = (props) => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </Typography>
       {props.owner &&
-        <Avatar style={{
-          margin: 10,
+        <Avatar sx={{
+          margin: '5px 0 5px 10px',
           width: "26px",
           height: "26px",
           alignSelf: "flex-end",
@@ -68,10 +77,10 @@ const TextMessage = (props) => {
 }
 
 const ConversationChat = (props) => {
-
   const [textMessage, setTextMessage] = useState("")
   const [checkedMessages, setCheckedMessages] = useState([])
   const [clickActionButton, setClickActionButton] = useState(false)
+  const chatRef = useRef()
 
   const onCloseConversation = (conversation) => {
     props.onCloseConversation(conversation)
@@ -82,6 +91,12 @@ const ConversationChat = (props) => {
   }
 
   const onAddImage = () => {
+
+  }
+  const onAddSnippet = () => {
+
+  }
+  const onAddTextPlaceholder = () => {
 
   }
 
@@ -111,125 +126,106 @@ const ConversationChat = (props) => {
   }
 
   return (
-    <Grid item xs>
-      <Stack sx={{//conversation chat
-        minWidth: "350px",
-        border: "#dadada solid 1px",
-      }}
-        key={props.conversation.id}
-      >
-        <Stack sx={{//header
-          padding: "20px",
-          backgroundColor: "#3871DA",
-        }}
-          direction="row"
-          flexWrap="nowrap"
-          alignItems="center"
-          justifyContent="space-between"
+    <Draggable draggableId={props.conversation.id} index={props.index} isDragDisabled={props.isPinned}>
+      {(provided) => (
+        <Paper
+          {...provided.draggableProps}
+          sx={{ height: '100%', minHeight: 0, display: 'flex', flexDirection: 'column' }}
+          ref={provided.innerRef}
         >
-          <Stack
-            spacing={2}
+
+          <Stack //header
+            {...provided.dragHandleProps}
+            p='20px'
+            bgcolor={props.isPinned ? "#3871DA80" : '#3871DAFF'}
             direction="row"
+            flexWrap="nowrap"
             alignItems="center"
+            gap={2}
+            color="#fff"
+            sx={{ userSelect: 'none' }}
           >
+            <IconButton onClick={()=>props.onPin()} color='inherit' size='small'>
+              <PushPinIcon />
+            </IconButton>
+
             <Avatar style={{
-              width: "36px",
-              height: "36px",
+              width: "34px",
+              height: "34px",
             }}
-              aria-label="recipe"
+              aria-label="avatar"
               src='https://stakdsocial.s3.us-east-2.amazonaws.com/media/general/contact-missing-image.png'
             />
-            <Typography style={{
-              color: "#ffff",
-              fontWeight: 500,
-            }}
-              variant="body1"
-            >
+            <Typography fontWeight={600}>
               {props.conversation.name}
             </Typography>
-            <Typography style={{
-              color: "#dadada",
-            }}
-              variant="subtitle2"
-            >
+            <Typography variant="subtitle2" >
               @charles
             </Typography>
+            <IconButton onClick={() => onCloseConversation(props.conversation)} size='small' color='inherit' sx={{ ml: 'auto' }} >
+              <CloseIcon />
+            </IconButton>
           </Stack>
-          <CloseIcon sx={{
-            color: "#ffff",
-            cursor: "pointer",
-          }}
-            onClick={() => onCloseConversation(props.conversation)}
-          />
-        </Stack>
 
-        {/* box textarea */}
-        <Stack p="20px">
-          <MessageInput
-            style={{
-              position: "relative",
-              width: "100%",
-              height: "110px",
-            }}
-            type='chat'
-            hideLabel
-            placeholder='Type your message here'
-            value={textMessage}
-            onChange={onTextAreaChange}
-            footerText={`${props.conversation.name} is typing...`}
-          />
+          {/* box textarea */}
+          <Box p="20px">
+            <TextField
+              fullWidth
+              multiline
+              rows={4}
+              inputRef={chatRef}
+              variant="outlined"
+              placeholder="Type your message here"
+              value={textMessage}
+              onChange={(e) => onTextAreaChange(e.target.value)}
+            />
 
-          <Stack sx={{
-            marginTop: "30px",
-          }}
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Box display="flex" flex={1}>
-              <InsertPhotoIcon
-                sx={{
-                  cursor: "pointer"
+            <Stack
+              mt={3}
+              direction="row"
+              alignItems="center"
+              gap={1.5}
+            >
+              <EmojiPicker
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  position: 'relative',
                 }}
-                onClick={onAddImage}
+                onEmojiSelected={(emoji) => {
+                  setTextMessage(textMessage + emoji.native)
+                  chatRef.current.focus()
+                }}
               />
+
+              <Divider flexItem orientation="vertical" />
+
+              <IconButton onClick={onAddImage} >
+                <InsertPhotoOutlinedIcon />
+              </IconButton>
+
+              <Divider flexItem orientation="vertical" />
+
+              <IconButton onClick={onAddTextPlaceholder} >
+                <WrapTextIcon />
+              </IconButton>
+
               <Divider
                 flexItem
                 orientation="vertical"
-                style={{ marginLeft: "10px", marginRight: "10px" }}
               />
 
-              <AutoFixHighIcon
-                sx={{
-                  cursor: "pointer"
-                }}
-              // onClick={ }
-              />
-              <Divider
-                flexItem
-                orientation="vertical"
-                style={{ marginLeft: "10px", marginRight: "10px" }}
-              />
-              <ContentCutIcon
-                sx={{
-                  cursor: "pointer"
-                }}
-              // onClick={ }
-              />
-            </Box>
-            <Button name="Send" variant="contained" />
-          </Stack>
+              <IconButton onClick={onAddSnippet} >
+                <ShortTextIcon />
+              </IconButton>
 
-        </Stack>
+              <Button name="Send" variant="contained" sx={{ ml: 'auto' }} />
+            </Stack>
+          </Box>
 
-        <Divider />
-
-        <Stack flex={2}>
+          <Divider />
 
           <Stack /* actions */
-            sx={{
-              width: "100%",
-            }}
             direction="row"
             justifyContent="space-between"
           >
@@ -261,7 +257,14 @@ const ConversationChat = (props) => {
             }
           </Stack>
 
-          <List style={{}}>
+          <List // messages list
+            sx={{
+              flex: '1 0 0',
+              overflowY: 'auto',
+              overflowX: 'hidden',
+              overscrollBehaviorBlock: 'contain',
+            }}
+          >
             {/* {props.conversation?.messages.map(message => ( */}
             <TextMessage
               owner
@@ -274,13 +277,51 @@ const ConversationChat = (props) => {
               onCheck={onCheckMessages}
               actionActive={clickActionButton}
             />
-            {/* ))
-          } */}
+            <TextMessage
+              owner
+              onCheck={onCheckMessages}
+              // message={message}
+              actionActive={clickActionButton}
+            />
+            <TextMessage
+              // message={message}
+              onCheck={onCheckMessages}
+              actionActive={clickActionButton}
+            />          <TextMessage
+              owner
+              onCheck={onCheckMessages}
+              // message={message}
+              actionActive={clickActionButton}
+            />
+            <TextMessage
+              // message={message}
+              onCheck={onCheckMessages}
+              actionActive={clickActionButton}
+            />          <TextMessage
+              owner
+              onCheck={onCheckMessages}
+              // message={message}
+              actionActive={clickActionButton}
+            />
+            <TextMessage
+              // message={message}
+              onCheck={onCheckMessages}
+              actionActive={clickActionButton}
+            />          <TextMessage
+              owner
+              onCheck={onCheckMessages}
+              // message={message}
+              actionActive={clickActionButton}
+            />
+            <TextMessage
+              // message={message}
+              onCheck={onCheckMessages}
+              actionActive={clickActionButton}
+            />
           </List>
-        </Stack>
-
-      </Stack>
-    </Grid>
+        </Paper>
+      )}
+    </Draggable>
   )
 }
 
