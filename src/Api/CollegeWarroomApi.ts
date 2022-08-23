afe593700f6d10866494c7d9fc9e38af1cd2cb2b
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2022-08-23 17:21:07 -0300

    event search interface

--- src/Api/CollegeWarroomApi.ts
+++ src/Api/CollegeWarroomApi.ts
@@ -3,12 +3,12 @@ import axios from "axios";
 const USERNAME = "RecruitSuiteAPI"
 const PASSWORD = "YodaUsesRecruitSuiteToo!"
 
-interface IfilterParameters {
+interface IfilterParametersSearch {
     access_token: string,
     _startRow?: string,
     _endRow?: string,
     _sortBy?: string,
-    high_school_ID?: string,
+    high_school_ID?: number,
     name?: string,
     address?: string,
     country?: string,
@@ -16,6 +16,21 @@ interface IfilterParameters {
     state?: string,
     postal_code?: string,
     ets_code?: string,
+}
+
+interface IfilterParametersEventSearch {
+    access_token: string,
+    _startRow?: string,
+    _endRow?: string,
+    _sortBy?: string,
+    high_school_ID?: number,
+    name?: string,
+    location?: string,
+    event_time?: Date,
+    home_team_name?: string,
+    away_team_name?: string,
+    team1_high_school_ID?: string | number,
+    team2_high_school_ID: string | number,
 }
 
 const CollegeWarroomApi = axios.create({
@@ -28,7 +43,7 @@ export const LoginCollegeWarroomApi = async () => {
     console.log(data) // save access_token to use in other requests
 }
 
-export const SearchCollegeWarroomApi = async (filterParameters: IfilterParameters) => {
+export const SearchCollegeWarroomApi = async (filterParameters: IfilterParametersSearch) => {
     const response = await CollegeWarroomApi.get("High_School/Search", {
         data: filterParameters,
     })
@@ -36,7 +51,7 @@ export const SearchCollegeWarroomApi = async (filterParameters: IfilterParameter
     console.log(data)
 }
 
-export const ScheduleCollegeWarroomApi = async (filterParameters: IfilterParameters) => {
+export const ScheduleCollegeWarroomApi = async (filterParameters: IfilterParametersEventSearch) => {
     const response = await CollegeWarroomApi.get("High_School_Event/Search ", {
         data: filterParameters, // 
     })
