27a9a2fd041865f663a888a3671e2a0df5eee06e
gaschneidr <gaschneider2008@hotmail.com>
2022-08-25 17:04:01 -0300

    Fixed panel filter not showing

---
+++ src/Pages/testData.js
@@ -0,0 +1,25 @@
+const dataset = [
+    {
+        status: 'test a',
+        id: 1,
+        tag: {
+            _status_test_a: true,
+        }
+    },
+    {
+        status: 'test b',
+        id: 2,
+        tag: {
+            _status_test_b: true,
+        }
+    },
+    {
+        status: 'test a',
+        id: 3,
+        tag: {
+            _status_test_a: true,
+        }
+    }
+]
+
+export default dataset
