f73e9962c03154dbe5f5c34b90650969e07bdfaf
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2022-09-02 11:33:12 -0300

    add interface

--- src/Interfaces/Shared.ts
+++ src/Interfaces/Shared.ts
@@ -5,6 +5,39 @@ export interface IPaginationApi {
     totalItems: number;
 }
 
+export interface ITwitterProfile {
+    screen_name: string;
+    profile_image: string;
+}
+
+export interface ILogo {
+    thumb: string;
+    medium: string;
+    large: string;
+    original: string;
+}
+
+export interface IOrg {
+    id: string;
+    name: string;
+    nickname: string;
+    mascot?: any;
+    logo: ILogo;
+    primary_color: string;
+    secondary_color: string;
+}
+
+export interface ITeam {
+    id: number;
+    name: string;
+    role: string;
+    org: IOrg;
+}
+
+export interface ISettings {
+    selected_columns: string[];
+}
+
 export interface IFilterOption<T> {
     itemLabel: string;
     value: T;
