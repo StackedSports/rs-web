c2ad2a7984452353d660ee761ea8438c9a73ddf8
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2022-09-27 09:53:49 -0300

    improve media grid skeleton

--- src/UI/Tables/Media/MediaGrid.js
+++ src/UI/Tables/Media/MediaGrid.js
@@ -8,43 +8,25 @@ export default function MediaGrid(props) {
     return (
         <Grid container spacing={2}>
             {
-                props.isLoading ? (
-                    Array.from(new Array(skeletonSize)).map((_, index) =>
-                        <Grid key={index}
-                            item
-                            xs={props.xs || 12}
-                            sm={props.sm || 6}
-                            md={props.md || 4}
-                            lg={props.lg || 3}
-                            xl={props.xl || 2}>
-                            <MediaPreview
-                                isLoading={props.isLoading}
-                                type={props.type}
-                                item={{}}
-                            />
-                        </Grid>
-                    )
-                ) : (
-                    props.items && props.items.map((item, index) =>
-                        <Grid key={(item.hashid || item.id) + (item.name || item.file_name)}
-                            item
-                            xs={props.xs || 12}
-                            sm={props.sm || 6}
-                            md={props.md || 4}
-                            lg={props.lg || 3}
-                            xl={props.xl || 2}>
-                            <MediaPreview
-                                isLoading={props.isLoading}
-                                type={props.type}
-                                item={item}
-                                linkTo={props.linkTo && `${props.linkTo}/${item.id}`}
-                                selected={props.selectionModel?.includes(item.id) || false}
-                                onSelectedChange={(selected) => props.onSelectedChange(selected, index, item)}
-                                onSendClick={props.onSendClick && (() => props.onSendClick(item))}
-                                onPreviewClick={props.onPreviewClick && ((item) => props.onPreviewClick(item))}
-                            />
-                        </Grid>
-                    )
+                Array.from(props.items?.length > 0 ? props.items : new Array(skeletonSize)).map((item, index) =>
+                    <Grid key={(item?.hashid || item?.id) || index}
+                        item
+                        xs={props.xs || 12}
+                        sm={props.sm || 6}
+                        md={props.md || 4}
+                        lg={props.lg || 3}
+                        xl={props.xl || 2}>
+                        <MediaPreview
+                            isLoading={props.isLoading}
+                            type={props.type}
+                            item={item || {}}
+                            linkTo={props.linkTo && `${props.linkTo}/${item?.id}`}
+                            selected={props.selectionModel?.includes(item?.id) || false}
+                            onSelectedChange={(selected) => props.onSelectedChange(selected, index, item)}
+                            onSendClick={props.onSendClick && (() => props.onSendClick(item))}
+                            onPreviewClick={props.onPreviewClick && ((item) => props.onPreviewClick(item))}
+                        />
+                    </Grid>
                 )
             }
         </Grid>
