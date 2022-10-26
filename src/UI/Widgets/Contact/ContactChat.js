6cf5269c8bb85400b3635c993c113087f97a78e9
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2022-10-26 11:58:05 -0300

    add sms contact chat

--- src/UI/Widgets/Contact/ContactChat.js
+++ src/UI/Widgets/Contact/ContactChat.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import lodash from "lodash";
 
 import { Stack } from "@mui/material";
@@ -6,35 +6,34 @@ import ContactChatHeader from "./ContactChatHeader";
 import { useContactConversation, useInboxConversationInfinty } from 'Api/ReactQuery'
 import { MessagesDisplay } from "UI/Widgets/Chat/MessagesDisplay";
 import { ChatInput } from "UI/Widgets/Chat/ChatInput";
+import { AuthContext } from "Context/Auth/AuthProvider";
 
 
 const ContactChat = (props) => {
+  const { user } = useContext(AuthContext)
+  const [conversationType, setConversationType] = useState('dm') //"dm" | "sms"
+  const messages = useInboxConversationInfinty({
+    contact_id: props.contact?.id,
+    inbox_type: conversationType,
+  })
 
-  const contactConversation = useContactConversation(props.contact?.id)
-  const contatcSmSConversation = useInboxConversationInfinty({ inbox_type: "sms", user_id: props.contact?.id })
-  const [loadedMessages, setLoadedMessages] = useState({})
-
-  useEffect(() => {
-    if (!contactConversation.loading) {
-      setLoadedMessages(prev => {
-        const prevMessages = prev?.messages || []
-        const newMessages = contactConversation.items?.messages || []
-        return {
-          ...contactConversation.items,
-          messages: lodash.uniqBy([...prevMessages, ...newMessages], "id").reverse()
-        }
-      })
+  const loadNextPageMessages = () => {
+    if (messages.hasNextPage && !messages.isFetching) {
+      messages.fetchNextPage()
     }
-  }, [contactConversation.items])
+  }
 
-  const hasMorePages = contactConversation.pagination.currentPage < contactConversation.pagination.currentPage.totalPages
-
-  const handleOnScrollEnd = () => {
-    const pagination = contactConversation.pagination
-    const loading = contactConversation.loading
-    const { currentPage, totalPages, getPage } = pagination;
-    if (hasMorePages && !loading) {
-      getPage(currentPage + 1)
+  const handlePlatformChange = (type) => {
+    switch (type) {
+      case 'twitter-dm':
+        setConversationType('dm')
+        break;
+      case 'rs-text':
+        setConversationType('sms')
+        break;
+      default:
+        setConversationType(null)
+        break;
     }
   }
 
@@ -43,13 +42,14 @@ const ContactChat = (props) => {
       flex={1}
       spacing={1}
     >
-      <ContactChatHeader contact={props.contact} onPlatformChange={(id) => console.log(id)} />
+      <ContactChatHeader contact={props.contact} onPlatformChange={handlePlatformChange} />
       <MessagesDisplay
-        contact_profile_image={loadedMessages.contact_profile_image}
-        coach_profile_image={loadedMessages.coach_profile_image}
-        messages={loadedMessages.messages || []}
-        onLoadMore={handleOnScrollEnd}
-        hasMore={hasMorePages}
+        contact_profile_image={props.contact?.twitter_profile?.profile_image}
+        coach_profile_image={user?.twitter_profile?.profile_image}
+        messages={messages.items}
+        onLoadMore={loadNextPageMessages}
+        loading={messages.isLoading}
+        hasMore={messages.hasNextPage || messages.isLoading}
       />
       <ChatInput />
     </Stack>
